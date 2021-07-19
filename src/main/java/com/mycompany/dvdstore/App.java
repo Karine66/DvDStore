ff3af99043a3cf9aeb223d5e6997e606346c38ff
Karine66 <shon@live.fr>
2021-07-19 16:29:23 +0200

    set up springboot

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -2,24 +2,21 @@ package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
 import com.mycompany.dvdstore.service.DefaultMovieService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan({"com.mycompany.dvdstore.repository.file","com.mycompany.dvdstore.controller"/*,"com.mycompany.dvdstore.service"*/})
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App
 {
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+       // ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
-    }
-    @Bean
-    public DefaultMovieService configureDefaultService() {
-        return new DefaultMovieService();
     }
 }
